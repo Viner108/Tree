c998c889069b23d9c9ddc51ce91605dfbf4a6f06
Viner108 <andrukovichl@mail.ru>
2023-05-20 19:51:08 +0300

    Rename methods checkingConditionsAndAdding, addItem and checkingBranches commit

--- src/main/java/org/example/ObjectTree.java
+++ src/main/java/org/example/ObjectTree.java
@@ -15,27 +15,28 @@ public class ObjectTree {
 
     public void add(ObjectTreeItem item, ObjectTreeItem root1) {
         startAdding = true;
-        addItem(item, root1);
+        checkingConditionsAndAdding(item, root1);
     }
 
-    private void addItem(ObjectTreeItem item, ObjectTreeItem root1) {
+    private void checkingConditionsAndAdding(ObjectTreeItem item, ObjectTreeItem root1) {
         if (!startAdding) {
             return;
         }
         int i = 0;
         if (item.payload.sum < root1.payload.sum) {
-            extracted(item, root1, i);
+            addItem(item, root1, i);
+
         } else {
             i++;
-            extracted(item, root1, i);
+            addItem(item, root1, i);
         }
 
         count++;
     }
 
-    private void extracted(ObjectTreeItem item, ObjectTreeItem root1, int i) {
+    private void addItem(ObjectTreeItem item, ObjectTreeItem root1, int i) {
         if (root1.items[i] != null) {
-            addItem(item, root1.items[i]);
+            checkingConditionsAndAdding(item, root1.items[i]);
         } else {
             root1.items[i] = item;
             startAdding = false;
@@ -47,16 +48,16 @@ public class ObjectTree {
         if (item.payload.sum == root2.payload.sum) {
             return true;
         } else if (item.payload.sum < root2.payload.sum) {
-            if (extracted2(item, root2, i)) return true;
-        }else {
+            if (checkingBranches(item, root2, i)) return true;
+        } else {
             i++;
-            if (extracted2(item, root2, i)) return true;
+            if (checkingBranches(item, root2, i)) return true;
         }
         return false;
     }
 
-    private boolean extracted2(ObjectTreeItem item, ObjectTreeItem root2, int i) {
-        if(root2.items[i]!=null&&searchItem(item, root2.items[i])) {
+    private boolean checkingBranches(ObjectTreeItem item, ObjectTreeItem root2, int i) {
+        if (root2.items[i] != null && searchItem(item, root2.items[i])) {
             return true;
         }
         return false;
